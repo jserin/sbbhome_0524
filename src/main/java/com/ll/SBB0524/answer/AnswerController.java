d2615153ad48e37de75b8688bf2eee7f132905a0
Jserin <jsr3643@naver.com>
2023-08-01 16:10:41 +0900

    whyrano

--- src/main/java/com/ll/SBB0524/answer/AnswerController.java
+++ src/main/java/com/ll/SBB0524/answer/AnswerController.java
@@ -54,6 +54,8 @@ public class AnswerController {
         answerForm.setContent(answer.getContent());
         return "answer_form";
     }
+
+
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
     public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
@@ -66,9 +68,9 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s#answer_%s",
-                answer.getQuestion().getId(), answer.getId());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
     public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
