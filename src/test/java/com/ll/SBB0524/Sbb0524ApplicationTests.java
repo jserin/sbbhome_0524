dc4e799a1b4b1c49e755656e3e042bd06f732cb0
Jserin <jsr3643@naver.com>
2023-08-02 11:43:32 +0900

    why

--- src/test/java/com/ll/SBB0524/Sbb0524ApplicationTests.java
+++ src/test/java/com/ll/SBB0524/Sbb0524ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.ll.SBB0524;
 
-import com.ll.SBB0524.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
